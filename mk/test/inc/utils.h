5e7f032aa37814108cf721247942b533d824113e
pecuyu <pecuyu9999@gmail.com>
2018-04-20 15:45:08 +0800

    add utils

---
+++ mk/test/inc/utils.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int add(int a,int b);
+int sub(int a,int b);
+int mul(int a,int b);
+int div(int a,int b);
+
+
+
+
--- mk/test/src/main.c
+++ mk/test/src/main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include"utils.h"
 
 void main()
 {
 
 	printf("hello mk\n");
-
+	int a=3,b=4;
+	printf("get sum :  %d + %d =  %d\n",a,b,add(a,b));
 }
